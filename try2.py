00afe741ca8f4c4e8e4b545e58da8b40ae1363a2
dharamshishchauhan <dharamshishchauhan@gmail.com>
2025-02-17 22:21:59 +0530

    new try2 file

--- try2.py
+++ try2.py
@@ -1,11 +1,9 @@
-# first-algo-demo-code
-#This is my first git repository
-
 import math
 import os
+
 ltp_banknifty_at_916am=240
 
 step=100
+
 nearest_str22 = round(ltp_banknifty_at_916am/step)*step
 print(nearest_str22)
-print("hi")
